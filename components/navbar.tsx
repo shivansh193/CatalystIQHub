420fb8d1b64a32755080957d050bf0e8c11588bd
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2024-06-05 01:06:36 +0530

    on wednesdays we wear pink

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
+    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-pink-950/50">
       <div className="w-full max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-14 items-center">
           <Link href="#" className="flex items-center" prefetch={false}>
